c1f229a7a5cb8a1cdee42edd350ff149b9617ec0
“bzhang420” <bzhang420@gatech.edu>
2024-08-19 15:58:12 -0400

    implmented save_deal, get_save_deal, and delete_save_deal functions

--- public/profile.js
+++ public/profile.js
@@ -2,108 +2,224 @@ let saved_deals = [];
 let saveURL = document.getElementById("shortenURL");
 
 function urlShortener(userURL) {
-    let result;
-    let query = "url=" + userURL;
-    let url = "/shortenURL";
-    let xhr = new XMLHttpRequest(); //listing for a request; template for letter 
-    xhr.responseType = 'json'; // type of request we're requesting
-    xhr.open("POST", url); // getting url  
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    
-    xhr.onload = function() {
-        var shortenerResponse = xhr.response;
-        copyToClipboard(shortenerResponse["shortURL"]);
-    }
-    
-    xhr.send(query)
-    
+  let result;
+  let query = "url=" + userURL;
+  let url = "/shortenURL";
+  let xhr = new XMLHttpRequest(); //listing for a request; template for letter
+  xhr.responseType = "json"; // type of request we're requesting
+  xhr.open("POST", url); // getting url
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  xhr.onload = function () {
+    var shortenerResponse = xhr.response;
+    copyToClipboard(shortenerResponse["shortURL"]);
+  };
+
+  xhr.send(query);
 }
 
 function copyToClipboard(text) {
-    var dummy = document.createElement("textarea");
-    // to avoid breaking orgain page when copying more words
-    // cant copy when adding below this code
-    dummy.style.display = 'none'
-    document.body.appendChild(dummy);
-    dummy.value = text;
-    dummy.select();
-    navigator.clipboard.writeText(dummy.value);
-    document.body.removeChild(dummy);
+  var dummy = document.createElement("textarea");
+  // to avoid breaking orgain page when copying more words
+  // cant copy when adding below this code
+  dummy.style.display = "none";
+  document.body.appendChild(dummy);
+  dummy.value = text;
+  dummy.select();
+  navigator.clipboard.writeText(dummy.value);
+  document.body.removeChild(dummy);
 }
 
-function sleep (millisec) { 
-    return new Promise(resolve=> {
-        setTimeout(resolve, millisec);
-    }) 
+function sleep(millisec) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, millisec);
+  });
 }
 
 function get_saved_deals() {
+  let xhr = new XMLHttpRequest();
+  url = `/get_saved_deals`;
+  xhr.responseType = "json";
+  xhr.open("GET", url);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-    let xhr = new XMLHttpRequest;
-    url = `/get_saved_deals`
-    xhr.responseType = "json"
-    xhr.open("GET", url)
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-    
-    xhr.onload = function() {
-        var dealsResponse = xhr.response;
-        dealsResponse.saved_deals.forEach(deal_info => {
-            saved_deals.push(deal_info);
-        });
+  xhr.onload = function () {
+    var dealsResponse = xhr.response;
+    dealsResponse.saved_deals.forEach((deal_info) => {
+      saved_deals.push(deal_info);
+    });
 
-        saved_deals.forEach(deal => {
-            render_saved_deals(deal);
-        })
-    }
+    saved_deals.forEach((deal) => {
+      render_saved_deals(deal);
+    });
+  };
 
-    xhr.send()
+  xhr.send();
+}
 
-} 
+// function render_saved_deals(deal_info) {
+//     sleep(500);
+//     let cardDetails ='<div class="card custom-card" style="width: 18rem;">' +
+//     '<img src="'+ deal_info.banner_link +'" class="card-img-top custom-card-image">' +
+//     '<div class="card-body custom-card-body">'+
+//     '<h5 class="card-title">' + deal_info.game_name + '</h5>'+
+//     '<a target="_blank" class="btn btn-primary custom-btn1" href="' + deal_info.deal_link +'">Deal Link</a>' +
+//     '<p class="card-text"><p>' + deal_info.retail_price + '</p><p>' + deal_info.price + '</p></p>' +
+//     '<a href="#" class="btn btn-primary custom-btn2" id="shortenURL" onclick="urlShortener(\''+ deal_info.deal_link + '\')">Copy Link</a>' +
+//     '<a href="#" class="btn btn-primary custom-btn2" onclick="delete_saved_deal(this.parentElement)">Delete Deal</a>'+
+//     '<p style="display: none;">'+ deal_info.id +'</p>'+
+//     '</div>' +
+//     '</div>';
+//     document.querySelector(".results").innerHTML += cardDetails;
+// }
 
 function render_saved_deals(deal_info) {
-    sleep(500);
-    let cardDetails ='<div class="card custom-card" style="width: 18rem;">' +
-    '<img src="'+ deal_info.banner_link +'" class="card-img-top custom-card-image">' + 
-    '<div class="card-body custom-card-body">'+
-    '<h5 class="card-title">' + deal_info.game_name + '</h5>'+
-    '<a target="_blank" class="btn btn-primary custom-btn1" href="' + deal_info.deal_link +'">Deal Link</a>' +
-    '<p class="card-text"><p>' + deal_info.retail_price + '</p><p>' + deal_info.price + '</p></p>' +
-    '<a href="#" class="btn btn-primary custom-btn2" id="shortenURL" onclick="urlShortener(\''+ deal_info.deal_link + '\')">Copy Link</a>' +
-    '<a href="#" class="btn btn-primary custom-btn2" onclick="delete_saved_deal(this.parentElement)">Delete Deal</a>'+
-    '<p style="display: none;">'+ deal_info.id +'</p>'+
-    '</div>' +
-    '</div>';
-    document.querySelector(".results").innerHTML += cardDetails;
+  sleep(500);
+  let cardDetails =
+    '<div class="card custom-card" style="width: 18rem;">' +
+    '<img src="' +
+    deal_info.banner_link +
+    '" class="card-img-top custom-card-image">' +
+    '<div class="card-body custom-card-body">' +
+    '<h5 class="card-title">' +
+    deal_info.game_name +
+    "</h5>" +
+    '<a target="_blank" class="btn btn-primary custom-btn1" href="' +
+    deal_info.deal_link +
+    '">Deal Link</a>' +
+    '<p class="card-text"><p>' +
+    deal_info.retail_price +
+    "</p><p>" +
+    deal_info.price +
+    "</p></p>" +
+    '<a href="#" class="btn btn-primary custom-btn2" id="shortenURL" onclick="urlShortener(\'' +
+    deal_info.deal_link +
+    "')\">Copy Link</a>" +
+    '<a href="#" class="btn btn-primary custom-btn2" onclick="delete_saved_deal(this)">Delete Deal</a>' +
+    // Hidden input element for the deal ID
+    '<input type="hidden" class="deal-id" value="' +
+    deal_info.deal_id +
+    '">' +
+    "</div>" +
+    "</div>";
+  document.querySelector(".results").innerHTML += cardDetails;
 }
 
-function sleep (millisec) { 
-    return new Promise(resolve=> {
-        setTimeout(resolve, millisec);
-    }) 
+function sleep(millisec) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, millisec);
+  });
 }
 
-function delete_saved_deal(deal_info) {
+// function delete_saved_deal(element) {
+//   // Access the parent card element to retrieve the hidden deal_id input
+//   const cardElement = element.closest(".card");
+//   const dealId = cardElement.querySelector(".deal-id").value;
 
-    let xhr = new XMLHttpRequest;
-    url = `/delete_saved_deal`
-    const query = `id=${deal_info.children[8].innerText}`
-    xhr.responseType = "json"
-    xhr.open("DELETE", url)
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-    
-    xhr.onload = function() {
-        var deleteResponse = xhr.response;
-        if (deleteResponse.success) {
-            deal_info.parentElement.remove();
-        }
+//   console.log(dealId);
+
+// //   if (dealId) {
+// //     // console.error("Deal ID is undefined or empty");
+// //     console.log("id found");
+// //   }
+
+//   let xhr = new XMLHttpRequest();
+//   const url = `/delete_saved_deal`;
+//   const query = `deal_id=${encodeURIComponent(dealId)}`;
+
+//   xhr.responseType = "json";
+//   xhr.open("DELETE", url);
+//   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+//   xhr.onload = function () {
+//     var deleteResponse = xhr.response;
+//     if (deleteResponse.success) {
+//       cardElement.remove(); // Remove the card from the DOM
+//     } else {
+//       console.error("Failed to delete deal:", deleteResponse.message);
+//     }
+//   };
+
+//   xhr.send(query);
+// }
+
+// function delete_saved_deal(element) {
+//   // Access the parent card element to retrieve the deal_id
+//   const cardElement = element.closest(".card");
+//   const dealId = cardElement.querySelector(".deal-id").value;
+
+//   if (!dealId) {
+//     console.error("Deal ID is undefined or empty");
+//     return;
+//   }
+
+//   let xhr = new XMLHttpRequest();
+//   const url = `/delete_saved_deal`;
+//   const query = `deal_id=${encodeURIComponent(dealId)}`;
+
+//   xhr.responseType = "json";
+//   xhr.open("DELETE", url);
+//   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+//   xhr.onload = function () {
+//     var deleteResponse = xhr.response;
+//     if (deleteResponse.success) {
+//       cardElement.remove();
+//       console.log("Deal deleted successfully, reloading page...");
+//       location.reload();
+//     } else {
+//       console.error("Failed to delete deal:", deleteResponse.message);
+//     }
+//   };
+
+//   xhr.onerror = function () {
+//     console.error("Request failed.");
+//   };
+
+//   xhr.send(query);
+// }
+
+function delete_saved_deal(element) {
+  // Access the parent card element to retrieve the deal_id
+  const cardElement = element.closest(".card");
+  console.log("cardelement:", cardElement);
+  const dealId = cardElement.querySelector(".deal-id").value;
+
+  if (!dealId) {
+    console.error("Deal ID is undefined or empty");
+    return;
+  }
+
+  let xhr = new XMLHttpRequest();
+  const url = `/delete_saved_deal`;
+  const query = `deal_id=${encodeURIComponent(dealId)}`;
+
+  xhr.responseType = "json";
+  xhr.open("DELETE", url);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  console.log("apple")
+
+  xhr.onload = function () {
+    console.log("here");
+    var deleteResponse = xhr.response;
+
+    console.log(deleteResponse);
+
+    if (deleteResponse.success) {
+      console.log("Deal deleted successfully, refetching deals...");
+      // Refetch the user's saved deals
+      cardElement.remove();
+    } else {
+      console.error("Failed to delete deal:", deleteResponse.message);
     }
+  };
 
-    xhr.send(query)
+  xhr.onerror = function () {
+    console.error("Request failed.");
+  };
 
-} 
-
-
+  xhr.send(query);
+}
 
 window.onload = get_saved_deals();
-
-    
--- server01.js
+++ server01.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 const bcrypt = require("bcryptjs");
 const costFactor = 10;
@@ -101,6 +103,96 @@ app.post("/attempt_login", async function (req, res) {
     console.error("Error:", err);
     res.json({ success: false, message: "Server Error" });
   }
+});
+
+app.post("/save_deal", function (req, res) {
+  if (authenticated) {
+    try {
+      const result = user_deal_db.insertOne({
+        username: currentUser,
+        deal_id: req.body.deal_id,
+        price: req.body.price,
+        retail_price: req.body.retail_price,
+        game_name: req.body.game_name,
+        deal_link: req.body.deal_link,
+        banner_link: req.body.banner_link,
+      });
+      res.json({ success: true, message: "Deal saved successfully" });
+    } catch (err) {
+      console.error("Error:", err);
+      res.json({ success: false, message: "Server Error" });
+    }
+  } else {
+    res.json({ success: false, message: "Not authenticated" });
+  }
+});
+
+app.get("/get_saved_deals", async function (req, res) {
+  if (authenticated) {
+    try {
+      const savedDeals = await user_deal_db
+        .find({ username: currentUser })
+        .toArray();
+      res.json({ success: true, saved_deals: savedDeals });
+    } catch (err) {
+      console.error("Error:", err);
+      res.json({ success: false, message: "Server Error" });
+    }
+  } else {
+    res.json({ success: false, message: "Not authenticated" });
+  }
+});
+
+
+app.delete("/delete_saved_deal", async function (req, res) {
+  console.log("Received DELETE request");
+
+  if (!authenticated) {
+    return res.json({ success: false, message: "Not authenticated" });
+  }
+
+  const dealId = req.body.deal_id;
+  const username = currentUser;
+
+  if (!dealId) {
+    return res.json({ success: false, message: "No deal ID provided" });
+  }
+
+  try {
+    // Use `await` to perform the delete operation
+    const result = await user_deal_db.deleteOne({
+      username: username,
+      deal_id: dealId,
+    });
+
+    if (result.deletedCount === 0) {
+      console.warn("No deal found with that ID for the current user");
+      return res.json({
+        success: false,
+        message: "No deal found with that ID for the current user",
+      });
+    } else {
+      console.log("Deal deleted successfully");
+      return res.json({ success: true, message: "Deal deleted successfully" });
+    }
+  } catch (err) {
+    console.error("Error occurred during deletion:", err);
+    return res.json({ success: false, message: "Cannot delete this deal" });
+  }
+});
+
+app.get("/profile", function (req, res) {
+  if (authenticated) {
+    res.sendFile(__dirname + "/public/" + "profile");
+  } else {
+    res.send("<p>not logged in <p><a href='/'>login page</a>");
+  }
+});
+
+app.get("/logout", function (req, res) {
+  authenticated = false;
+  currentUser = "";
+  res.sendFile(__dirname + "/public/" + "index.html");
 });
 
 // Start the Express server
